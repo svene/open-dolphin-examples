e18d5142c9f29778d29a06dc10bda3f787c7a935
Sven Ehrke <sven.ehrke@canoo.com>
2014-07-31 23:18:32 +0200

    working two way binding

--- od_web_angularjs_cso/js/app/main.js
+++ od_web_angularjs_cso/js/app/main.js
@@ -2,13 +2,20 @@ var app = angular.module('app', []);
 
 app.controller('DemoCtrl', function($scope) {
 
+	$scope.init = function()
+	{
+		console.log("controller.init");
+	};
+
 	$scope.firstName = "Sven";
 
     $scope.doit = function (data) {
         console.log("hallo " + $scope.firstName);
     };
 
+
 });
+
 
 app.directive('odbind', function($parse) {
 	return {
@@ -18,7 +25,7 @@ app.directive('odbind', function($parse) {
 			pm: '=',
 			attribute: '='
 		}
-		,link: function(scope, element, attrs){
+		,link: function(scope, element, attrs) {
 			scope.$watch('ngModel', function(newVal, oldVal) {
 				console.log("odbind: ", oldVal, " -> ",  newVal);
 				console.log("pm: ", attrs.pm);
@@ -38,6 +45,28 @@ app.directive('odbind', function($parse) {
 					}
 				}
 			});
+
+			// Bind value of attribute to ng-model:
+			console.log("3");
+
+			globalDolphin.getAt(attrs.pm).getAt(attrs.attribute).onValueChange(function (event) {
+				console.log("dolphin model changed: ", event);
+				if (event.newValue === null) {
+					console.log("newValue is null");
+					return;
+				}
+				if (event.newValue === event.oldValue) {
+					console.log("values are the same");
+					return;
+				}
+				if (event.newValue === scope.ngModel) {
+					console.log("dolphin and ng-model are the same");
+					return;
+				}
+				scope.ngModel = event.newValue;
+				scope.$apply();
+			});
+			console.log("4");
 		}
 	}
 });
