99195900e66e815275f712aca4483b33ef4bfa31
Sven Ehrke <sven.ehrke@canoo.com>
2014-08-28 09:46:24 +0200

    handling values for input field

--- od_reactjs_cso/app/example.js
+++ od_reactjs_cso/app/example.js
@@ -2,26 +2,27 @@
  * @jsx React.DOM
  */
 var ExampleApplication = React.createClass({
-  render: function() {
-    var elapsed = Math.round(this.props.elapsed  / 100);
-    var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
-    var message =
-      'React has been successfully running for ' + seconds + ' seconds.';
 
-    return <div className="container" role="main">
-		  <input id="nameTextField" type="text" value=""></input>
-			  <button id="greetButton" className="btn btn-primary">Greet</button>
-			  <h1><span id="greetingLabel" className="label label-primary label-success">unchanged</span></h1>
+	getInitialState: function(){
+		return {
+			name: 'Duke'
+		}
+	},
 
-		  </div>;
-  }
+	render: function () {
+
+		return <div className="container" role="main">
+			<input id="nameTextField" type="text" defaultValue={this.state.name} placeholder="placeholder"></input>
+			<button id="greetButton" className="btn btn-primary">Greet</button>
+			<h1>
+				<span id="greetingLabel" className="label label-primary label-success">Hello {name}</span>
+			</h1>
+
+		</div>;
+	}
 });
 
-var start = new Date().getTime();
-
-setInterval(function() {
-  React.renderComponent(
-    <ExampleApplication elapsed={new Date().getTime() - start} />,
-    document.getElementById('container')
-  );
-}, 50);
+React.renderComponent(
+<ExampleApplication/>,
+document.getElementById('container')
+);
