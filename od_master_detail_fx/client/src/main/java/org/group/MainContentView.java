4b6b46bbe8e2cbde6ed562a7cd89460d61889c92
Sven Ehrke <sven.ehrke@canoo.com>
2015-01-21 17:28:34 +0100

    ids and widgets for view

---
+++ od_master_detail_fx/client/src/main/java/org/group/MainContentView.java
@@ -0,0 +1,28 @@
+package org.group;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import org.opendolphin.core.PresentationModel;
+
+public class MainContentView {
+
+	@FXML
+	TableView<PresentationModel> personTable;
+
+	@FXML
+	TableColumn<PresentationModel, String> firstNameColumn;
+	@FXML
+	TableColumn<PresentationModel, String> lastNameColumn;
+
+	@FXML
+	TextField firstNameTextField;
+
+	@FXML
+	TextField lastNameTextField;
+
+	@FXML
+	TextField birthdayTextField;
+}
