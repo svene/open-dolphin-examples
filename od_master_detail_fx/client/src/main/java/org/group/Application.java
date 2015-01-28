e3532f3983e8308cc067d6dde287371ca49d2a55
Sven Ehrke <sven.ehrke@canoo.com>
2015-01-28 06:56:11 +0100

    started to integrate master-detail mechanism

--- od_master_detail_fx/client/src/main/java/org/group/Application.java
+++ od_master_detail_fx/client/src/main/java/org/group/Application.java
@@ -7,14 +7,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import org.opendolphin.core.client.ClientDolphin;
-import org.opendolphin.core.client.ClientPresentationModel;
-import org.opendolphin.core.client.comm.OnFinishedHandlerAdapter;
+import org.opendolphinx.client.misc.J8ClientSupport;
 import org.svenehrke.javafxextensions.fxml.FXMLLoader2;
 import org.svenehrke.javafxextensions.fxml.ViewAndRoot;
 
 import static org.group.ApplicationConstants.*;
-
-import java.util.List;
 
 public class Application extends javafx.application.Application {
     static ClientDolphin clientDolphin;
@@ -34,25 +31,16 @@ public class Application extends javafx.application.Application {
         stage.setScene(scene);
         stage.show();
 
-        addClientSideAction();
-        new MainContentViewBinder().bindView(clientDolphin, main.getView());
-        initializePMs();
+        MainContentViewBinder mainContentViewBinder = new MainContentViewBinder();
+        mainContentViewBinder.bindView(clientDolphin, main.getView());
+
+        initializePMs(() -> mainContentViewBinder.handleInitializedPMs(clientDolphin, main.getView()));
 
         stage.show();
     }
 
-    private void initializePMs() {
-		clientDolphin.send(COMMAND_INIT);
+    private void initializePMs(Runnable initializedHandler) {
+		clientDolphin.send(COMMAND_INIT, J8ClientSupport.onFinishedHandler( initializedHandler));
     }
 
-    private void addClientSideAction() {
-/*
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                clientDolphin.send(COMMAND_GREET);
-            }
-        });
-*/
-    }
 }
--- od_master_detail_fx/client/src/main/java/org/group/MainContentViewBinder.java
+++ od_master_detail_fx/client/src/main/java/org/group/MainContentViewBinder.java
@@ -2,12 +2,14 @@ package org.group;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import org.opendolphin.binding.JFXBinder;
 import org.opendolphin.core.PresentationModel;
 import org.opendolphin.core.client.ClientAttribute;
 import org.opendolphin.core.client.ClientDolphin;
 import org.opendolphin.examplepms.person.PersonApi;
 import org.opendolphinx.client.misc.ClientAttributeStringWrapper;
 import org.opendolphinx.client.misc.ModelStoreBinder;
+import org.opendolphinx.client.pattern.masterdetail.MasterDetailClientApi;
 
 public class MainContentViewBinder {
 
@@ -15,12 +17,22 @@ public class MainContentViewBinder {
 		// Initialize the person table:
 
 		ObservableList<PresentationModel> personPMs = FXCollections.observableArrayList();
-		new ModelStoreBinder(clientDolphin).bind(personPMs, PersonApi.PERSON_TYPE);
+		new ModelStoreBinder(clientDolphin).bind(personPMs, PersonApi.PERSON_TYPE); // todo: should these be provided via 'MasterDetailClientApi' ?
 
 		view.personTable.setItems(personPMs);
 
 		view.firstNameColumn.setCellValueFactory(cellData -> new ClientAttributeStringWrapper((ClientAttribute) cellData.getValue().getAt(PersonApi.ATT_FIRST_NAME)));
 		view.lastNameColumn.setCellValueFactory(cellData -> new ClientAttributeStringWrapper((ClientAttribute) cellData.getValue().getAt(PersonApi.ATT_LAST_NAME)));
 
+
+	}
+
+
+	public void handleInitializedPMs(ClientDolphin clientDolphin, MainContentView view) {
+		MasterDetailClientApi masterDetail = new MasterDetailClientApi(clientDolphin, PersonApi.PM_MASTER_DETAIL_PERSON_ID, PersonApi.PERSON_TYPE);
+		PresentationModel pm0 = masterDetail.findAllPresentationModels().get(0);
+		masterDetail.setCurrentPMId(pm0.getId());
+		JFXBinder.bind(PersonApi.ATT_FIRST_NAME).of(masterDetail.getCurrentItem()).to("text").of(view.firstNameTextField);
+		JFXBinder.bind("text").of(view.firstNameTextField).to(PersonApi.ATT_FIRST_NAME).of(masterDetail.getCurrentItem());
 	}
 }
