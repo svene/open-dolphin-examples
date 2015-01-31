ea573ac0b60a79b39e747343a0c198caf26b23d7
Sven Ehrke <sven.ehrke@canoo.com>
2015-01-31 15:19:15 +0100

    refactoring

--- od_example_pms/od-example-pms-server/src/main/java/org/opendolphin/examplepms/person/PersonSampleDataInitializer.java
+++ od_example_pms/od-example-pms-server/src/main/java/org/opendolphin/examplepms/person/PersonSampleDataInitializer.java
@@ -4,10 +4,13 @@ import org.opendolphin.core.server.ServerDolphin;
 
 public class PersonSampleDataInitializer {
 
+	public static final String PM_1 = "PM1";
+	public static final String PM_2 = "PM2";
+
 	public void initialize(ServerDolphin serverDolphin) {
 
-		PersonServerAPI.newPM(serverDolphin, "PM1", "Sven", "Ehrke", "31.10.1967");
-		PersonServerAPI.newPM(serverDolphin, "PM2", "Uli", "Tretter", "7.7.1969");
+		PersonServerAPI.newPM(serverDolphin, PM_1, "Sven", "Ehrke", "31.10.1967");
+		PersonServerAPI.newPM(serverDolphin, PM_2, "Uli", "Tretter", "7.7.1969");
 	}
 
 
