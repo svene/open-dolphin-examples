02192526e27c94e78971505ce262cd7bb88d5aaf
Sven Ehrke <sven.ehrke@canoo.com>
2015-05-25 12:05:30 +0200

    using servlet 3.0 features instead of web.xml

--- od_cors_example/server/src/main/java/com/mycompany/servlet/InvalidationServlet.java
+++ od_cors_example/server/src/main/java/com/mycompany/servlet/InvalidationServlet.java
@@ -2,10 +2,12 @@ package com.mycompany.servlet;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/dolphin/invalidate")
 public class InvalidationServlet extends HttpServlet {
 
     @Override
--- od_cors_example/webapp/src/main/java/com/mycompany/CorsFilter.java
+++ od_cors_example/webapp/src/main/java/com/mycompany/CorsFilter.java
@@ -1,12 +1,20 @@
 package com.mycompany;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@WebFilter(
+	urlPatterns = {"/*"},
+	initParams = {
+		@WebInitParam(name = "origin-whitelist", value = "http://localhost:63342,null")
+	}
+)
 public class CorsFilter implements Filter {
 
 	private List<String> originWhitelist = new ArrayList<>();
